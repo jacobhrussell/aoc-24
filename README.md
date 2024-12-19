b14b1e6a7ea07d1f743fe260705de705982fe9d8
jacobhrussell <jacobhenryrussell@gmail.com>
2024-12-19 11:17:13 -0500

    day five scaffold

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 https://adventofcode.com/
 
-Not finished:
+Not finished -- there be dragons:
 - Day 2 part 2
 - Day 3 part 2
 - Day 4
---
+++ src/main/java/org/example/day5/part1/Main.java
@@ -0,0 +1,25 @@
+package org.example.day5.part1;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> inputOne = new ArrayList<>();
+        InputStream inputStreamOne = Main.class.getResourceAsStream("/day5_1.txt");
+        InputStreamReader inputStreamReaderOne = new InputStreamReader(inputStreamOne);
+        BufferedReader bufferedReaderOne = new BufferedReader(inputStreamReaderOne);
+        inputOne = bufferedReaderOne.lines().toList();
+
+        List<String> inputTwo = new ArrayList<>();
+        InputStream inputStreamTwo = Main.class.getResourceAsStream("/day5_2.txt");
+        InputStreamReader inputStreamReaderTwo = new InputStreamReader(inputStreamTwo);
+        BufferedReader bufferedReaderTwo = new BufferedReader(inputStreamReaderTwo);
+        inputTwo = bufferedReaderTwo.lines().toList();
+
+    }
+}
